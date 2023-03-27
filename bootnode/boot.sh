9f462b62d9fc780c9ef2dd9ba7679ee35f37fc01
Sergey <szhdanoff@gmail.com>
2023-03-27 21:16:54 +0300

    add node

--- bootnode/boot.sh
+++ bootnode/boot.sh
@@ -3,34 +3,28 @@
 # PoA
 if [ ! -f ~/.poa1 ]; then
     echo "PoA running node..."
-    geth --datadir=/root/node/node-data init "/root/genesis.json"
+    geth --datadir=/root/node/bootnode init "/root/files/genesis.json"
     touch ~/.poa1
 fi
-## json-key оne-time migration
-#if [ -f ~/.fork2 ]; then
-#    echo "fork2 found, attempt key migration..."
-#    if [ ! -f ~/.migrated ]; then
-#      echo "Migrating json keys..."
-#      cp /root/node/joys/keystore/* /root/node/node-data/keystore/
-#    touch ~/.migrated
-#    fi
-#fi
 
-sleep 3
-cp /root/static-nodes.json /root/node/node-data/geth
 
-NODE_NAME=dockerp.${HOSTNAME}
+NODE_NAME=node.${HOSTNAME}
 
 geth \
---datadir=/root/node/node-data \
---networkid=35855456 \
+--datadir=/root/node/bootnode \
+--keystore /root/files/keystore \
+--networkid=1010101 \
+--syncmode=full \
+--gcmode=archive \
+--nodiscover \
 --http \
 --http.vhosts "*" \
 --http.corsdomain "*" \
 --http.addr 0.0.0.0 \
 --http.api "eth,net,web3,txpool" \
---port 31323 \
+--port 31513 \
 --nat "any" \
 --txlookuplimit=0 \
 --miner.gasprice 20000000000 \
---ethstats=${NODE_NAME}:12345@netstats:3000
+--ethstats=${NODE_NAME}:12345@netstats:3000 \
+--nodekeyhex=091bd6067cb4612df85d9c1ff85cc47f259ced4d4cd99816b14f35650f59c322
--- node/start.sh
+++ node/start.sh
@@ -1,58 +1,34 @@
 #!/usr/bin/env bash
 
-# deprecated PoW
-#
-#if [ ! -f ~/.initialised ]; then
-#    echo "First time running node..."
-#    geth --datadir=/root/node/joys init "/root/genesis.json"
-#    touch ~/.initialised
-#fi
-#if [ ! -f ~/.fork1 ]; then
-#    echo "fork1 running node..."
-#    geth --datadir=/root/node/joys init "/root/fork1.json"
-#    touch ~/.fork1
-#fi
-#
-#if [ ! -f ~/.fork2 ]; then
-#    echo "fork2 running node..."
-#    geth --datadir=/root/node/joys init "/root/fork2.json"
-#    touch ~/.fork2
-#fi
+NODE_NAME=node.${HOSTNAME}
 
-# PoAS
-if [ ! -f ~/.poas1 ]; then
-    echo "PoAS running node..."
-    geth --datadir=/root/node/joys-poas init "/root/joys-posa.json"
-    touch ~/.poas1
-fi
-# json-key оne-time migration
-if [ -f ~/.fork2 ]; then
-    echo "fork2 found, attempt key migration..."
-    if [ ! -f ~/.migrated ]; then
-      echo "Migrating json keys..."
-      cp /root/node/joys/keystore/* /root/node/joys-poas/keystore/
-    touch ~/.migrated
-    fi
+# PoA
+if [ ! -f ~/.poa1 ]; then
+    echo "PoA running node..."
+    geth --datadir=/root/node/${HOSTNAME} init "/root/files/genesis.json"
+    touch ~/.poa1
 fi
 
-sleep 3
-cp /root/static-nodes.json /root/node/joys-poas/geth
-
-NODE_NAME=dockerp.${HOSTNAME}
 
 geth \
---datadir=/root/node/joys-poas \
---networkid=35855456 \
+--datadir=/root/node/${HOSTNAME} \
+--keystore /root/files/keystore \
+--password /root/files/passwords/passwords.txt \
+--unlock "0xed9d02e382b34818e88b88a309c7fe71e65f419d" \
+--rpc.allow-unprotected-txs \
+--allow-insecure-unlock \
+--networkid=1010101 \
 --http \
 --http.vhosts "*" \
 --http.corsdomain "*" \
 --http.addr 0.0.0.0 \
 --http.api "eth,net,web3,txpool" \
 --bootnodes="\
-enode://98b954bb16ece6ad373a040f0ebac1b80e0bb4be36102ea6dcf7ea36ddd83f7ec72966727959d00bc8134bb7dd9a92cda533a3e28e8334c455f655c01704ed56@188.124.39.113:31323,\
-enode://fe09937a938b3747fcd9b89db94d5ae9e2d319751a8139085ed7f8cc79d4409c5cffc53807cf6db65b45c9e12b9754c3393e4db0ee90a3f9de2bc1ab6c5a3ba4@188.124.39.231:31323" \
---port 31323 \
+enode://288b97262895b1c7ec61cf314c2e2004407d0a5dc77566877aad1f2a36659c8b698f4b56fd06c4a0c0bf007b4cfb3e7122d907da3b005fa90e724441902eb19e@bootnode:31513" \
+--port 31513 \
 --nat "any" \
 --txlookuplimit=0 \
 --miner.gasprice 20000000000 \
---ethstats=${NODE_NAME}:q32399VqOO4n@poas.joys.digital:3000
+--ethstats=${NODE_NAME}:12345@netstats:3000 \
+--miner.etherbase="0xed9d02e382b34818e88b88a309c7fe71e65f419d" \
+--mine
